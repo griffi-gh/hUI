d430996e14a7ee7377db91cb90f6198249cffe6b
griffi-gh <prasol258@gmail.com>
2024-03-22 19:35:00 +0100

    impl from stuff

--- hui/src/frame.rs
+++ hui/src/frame.rs
@@ -1,5 +1,3 @@
-//TODO finish dis
-
 use glam::{Vec2, vec2};
 use derive_more::{Add, AddAssign, Sub, SubAssign};
 use crate::{
@@ -7,6 +5,8 @@ use crate::{
   element::fill_rect::FillRect,
   layout::{Size, Size2d}
 };
+
+//TODO finish dis, slider component would be a great place to test it
 
 /// Point inside a frame
 ///
@@ -19,6 +19,12 @@ pub struct FramePoint {
 
   /// Relative positioning
   pub relative: f32,
+}
+
+impl From<f32> for FramePoint {
+  fn from(value: f32) -> Self {
+    Self::absolute(value)
+  }
 }
 
 impl From<Size> for FramePoint {
@@ -76,9 +82,25 @@ impl FramePoint {
   }
 }
 
+#[derive(Clone, Copy, Debug, Default, Add, AddAssign, Sub, SubAssign)]
 pub struct FramePoint2d {
   pub x: FramePoint,
   pub y: FramePoint,
+}
+
+impl From<(FramePoint, FramePoint)> for FramePoint2d {
+  fn from((x, y): (FramePoint, FramePoint)) -> Self {
+    Self { x, y }
+  }
+}
+
+impl From<Size> for FramePoint2d {
+  fn from(size: Size) -> Self {
+    Self {
+      x: size.into(),
+      y: size.into(),
+    }
+  }
 }
 
 impl From<Size2d> for FramePoint2d {
@@ -86,6 +108,24 @@ impl From<Size2d> for FramePoint2d {
     Self {
       x: size.width.into(),
       y: size.height.into(),
+    }
+  }
+}
+
+impl From<(f32, f32)> for FramePoint2d {
+  fn from((x, y): (f32, f32)) -> Self {
+    Self {
+      x: FramePoint::absolute(x),
+      y: FramePoint::absolute(y),
+    }
+  }
+}
+
+impl From<Vec2> for FramePoint2d {
+  fn from(vec: Vec2) -> Self {
+    Self {
+      x: FramePoint::absolute(vec.x),
+      y: FramePoint::absolute(vec.y),
     }
   }
 }

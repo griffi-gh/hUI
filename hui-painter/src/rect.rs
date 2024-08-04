61989c7a79f109b798353324ea8432483c7653a8
griffi-gh <prasol258@gmail.com>
2024-08-04 21:04:27 +0200

    uwu

--- hui-painter/src/rect.rs
+++ hui-painter/src/rect.rs
@@ -2,12 +2,6 @@ use glam::Vec2;
 use hui_shared::rect::{Corners, FillColor};
 
 pub struct PaintRectParams {
-  /// Position of the top-left corner of the rectangle.
-  pub position: Vec2,
-
-  /// Position of the bottom-right corner of the rectangle.
-  pub size: Vec2,
-
   /// Color of the rectangle.
   pub color: FillColor,
 

1049ec12688032728c8015a5e017fc47cf00f77b
griffi-gh <prasol258@gmail.com>
2025-03-31 14:22:44 +0200

    wip hui-euc

--- hui-euc/src/lib.rs
+++ hui-euc/src/lib.rs
@@ -1,3 +1,34 @@
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 // TODO
+
+use euc::Pipeline;
+
+struct UiPipeline;
+
+impl Pipeline for UiPipeline {
+  type Vertex = [f32; 2];
+  type VsOut = ();
+  type Pixel = [u8; 4];
+
+  // Vertex shader
+  fn vert(&self, pos: &Self::Vertex) -> ([f32; 4], Self::VsOut) {
+    ([pos[0], pos[1], 0.0, 1.0], ())
+  }
+
+  // Fragment shader
+  fn frag(&self, _: &Self::VsOut) -> Self::Pixel {
+    [0, 0, 0, 255]
+  }
+}
+
+// fn main() {
+//   let mut color = Buffer2d::new([640, 480], [0; 4]);
+//   let mut depth = Buffer2d::new([640, 480], 1.0);
+
+//   Example.draw::<Triangles<_>, _>(
+//     &[[-1.0, -1.0], [1.0, -1.0], [0.0, 1.0]],
+//     &mut color,
+//     &mut depth,
+//   );
+// }
